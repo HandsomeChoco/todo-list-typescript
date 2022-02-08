e6043d88e76e96a506b621a3b70cc78d2738b9d8
HandsomeChoco <kkb931115@gmail.com>
2022-02-08 17:54:24 +0900

    컴포넌트 하나 렌더링 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './App.css';
 import GlobalThemeOverride from './components/GlobalThemeOverride';
 import AppContainer from './components/AppContainer';
+import PrintDate from './components/PrintDate';
 
 function App() {
   return (
     <GlobalThemeOverride>
       <AppContainer>
-        
+        <PrintDate/>
       </AppContainer>
     </GlobalThemeOverride>
-    
   );
 }
 
