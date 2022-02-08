d8ab45f7148e6a67dca6d71468f70b75fe55d433
HandsomeChoco <kkb931115@gmail.com>
2022-02-08 17:48:37 +0900

    컨테이너의 Padding 을 반응형으로 변경

--- src/components/AppContainer.tsx
+++ src/components/AppContainer.tsx
@@ -1,31 +1,61 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import PrintDate from './PrintDate';
-
+import { styled } from '@mui/material';
+import { standard } from '../constants/constants';
 interface AppContainerProps {
   children: React.ReactNode
 }
 
+const ResponsivePaddingContainer = styled(Container)(({ theme }) => {
+  return {
+    [theme.breakpoints.down("sm")]: {
+      paddingTop: standard * 3, // 24
+      paddingLeft: standard * 4, // 32
+      paddingRight: standard * 4, // 32
+      paddingBottom: standard * 0
+    },
+    [theme.breakpoints.up("sm")]: {
+      paddingTop: standard * 8, // 64
+      paddingLeft: standard * 9, // 72
+      paddingRight: standard * 9, // 72
+      paddingBottom: standard * 0
+    }
+  }
+});
+
+const ResponsivePaddingBox = styled(Box)(({ theme }) => {
+  return {
+    [theme.breakpoints.down("sm")]: {
+      paddingTop: standard * 4, // 24
+      paddingLeft: standard * 4, // 32
+      paddingRight: standard * 4, // 32
+      paddingBottom: standard * 0
+    },
+    [theme.breakpoints.up("sm")]: {
+      paddingTop: standard * 7, // 56
+      paddingLeft: standard * 7, // 56
+      paddingRight: standard * 7, // 56
+      paddingBottom: standard * 0
+    }
+  }
+});
+
 export default function AppContainer({ children }: AppContainerProps) {
   return (
     <React.Fragment>
-      <Container maxWidth="md" disableGutters>
-        <Box 
-          sx={{ 
+      <ResponsivePaddingContainer maxWidth="md" disableGutters>
+        <ResponsivePaddingBox 
+          sx={{
             bgcolor: '#ffffff',
             height: '100%',
-            paddingTop: 4,
-            paddingLeft: 3,
-            paddingRight: 3,
-            borderTopLeftRadius: 32, 
-            borderTopRightRadius: 32,
+            borderTopLeftRadius: standard * 4, 
+            borderTopRightRadius: standard * 4,
           }}
         >
-          <PrintDate/>
           { children }
-        </Box>
-      </Container>
+        </ResponsivePaddingBox>
+      </ResponsivePaddingContainer>
     </React.Fragment>
   );
 }
