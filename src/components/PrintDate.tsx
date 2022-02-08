22698b520f60632e2500793e8442debb30c46d93
HandsomeChoco <kkb931115@gmail.com>
2022-02-08 17:49:51 +0900

    * 버튼 추가 및 남은 할 일 개수 출력 * 컴포넌트에 전달할 sx 모두 외부로 옮김

--- src/components/PrintDate.tsx
+++ src/components/PrintDate.tsx
@@ -1,16 +1,55 @@
-import { Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import IconButton from '@mui/material/IconButton';
+import { appThemeColor, white } from "../constants/constants";
 
 const date = new Date();
-// Request a weekday along with a long date
-const options: Intl.DateTimeFormatOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+const options: Intl.DateTimeFormatOptions = { weekday: 'long', month: 'long', day: 'numeric' };
+
+const boxSx = { 
+  display: 'flex',
+  justifyContent: 'space-between',
+};
+
+const typoSx = { 
+  fontFamily: 'SUIT-Medium', 
+  display: 'flex', 
+  flexFlow: 'rows no-wrap', 
+  alignItems: 'center' 
+};
+
+const iconButtonSx = { 
+  backgroundColor: appThemeColor, 
+  color: white, 
+  width: 70, 
+  height: 70 
+};
+
+const iconSx = { fontSize: 35 };
+const remainTaskSx = { color: appThemeColor };
 
 function PrintDate() {
   const today = date.toLocaleString('ko-kr', options);
-  const currentTime = date.toLocaleTimeString('ko-kr');
+
   return(
-    <Typography variant="h2" sx={{ fontFamily: 'SUIT-Medium' }}>
-        {today}, <br></br> {currentTime}
-    </Typography>
+    <Stack>
+      <Box sx={boxSx}>
+        <Typography variant="h3" sx={typoSx}>
+          <strong>
+            {today}
+          </strong>
+        </Typography>
+        <IconButton sx={iconButtonSx}>
+          <AddIcon sx={iconSx}/>
+        </IconButton>
+      </Box>
+      <Box>
+        <Typography sx={remainTaskSx}>
+          3 tasks remain
+        </Typography>
+      </Box>
+    </Stack>
+      
   )
 }
 
