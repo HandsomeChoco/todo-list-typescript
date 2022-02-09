e3788506a3860e90bb3c62c193f173c597bb7eaa
HandsomeChoco <kkb931115@gmail.com>
2022-02-10 02:16:34 +0900

    * sx props 타입 지정 * 공휴일 데이터 가져오기 xml request 작성 * UI 출력 데이터 변경 및 UI 수정정

--- src/components/PrintDate.tsx
+++ src/components/PrintDate.tsx
@@ -1,42 +1,63 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Stack, SxProps, Typography } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import IconButton from '@mui/material/IconButton';
 import { appThemeColor, white } from "../constants/constants";
+import { useLayoutEffect } from "react";
+import axios from "axios";
 
-const date = new Date();
-const options: Intl.DateTimeFormatOptions = { weekday: 'long', month: 'long', day: 'numeric' };
+const date: Date = new Date();
+const options: Intl.DateTimeFormatOptions = { weekday: 'short', month: 'long', day: 'numeric' };
 
-const boxSx = { 
+const boxSx: SxProps = {
   display: 'flex',
   justifyContent: 'space-between',
 };
 
-const typoSx = { 
+const typoSx: SxProps = {
   fontFamily: 'SUIT-Medium', 
   display: 'flex', 
   flexFlow: 'rows no-wrap', 
-  alignItems: 'center' 
+  alignItems: 'center'
 };
 
-const iconButtonSx = { 
+const iconButtonSx: SxProps = {
   backgroundColor: appThemeColor, 
   color: white, 
-  width: 70, 
-  height: 70 
+  width: 60, 
+  height: 60 
 };
 
-const iconSx = { fontSize: 35 };
-const remainTaskSx = { color: appThemeColor };
+const iconSx: SxProps = { fontSize: 35 };
+const remainTaskSx: SxProps = { color: appThemeColor };
 
-function PrintDate() {
-  const today = date.toLocaleString('ko-kr', options);
+function PrintDate(): JSX.Element {
+  const [month, day, whatDate]: string[] = date.toLocaleString('ko-kr', options).split(' ');
+
+  const getHolidays = async() => {
+    const year = date.getFullYear();
+    let month: string | number  = date.getMonth() + 1
+
+    if (month < 10) {
+      month = '0'+ month
+    }
+
+    const response = await axios.get(`http://10.10.12.53:9000/api/${year}/${month}`);
+    console.log(response.data.response.body)
+    return response.data.response.body;
+  }
+
+  useLayoutEffect(() => {
+    getHolidays();
+
+  }, []);
 
   return(
     <Stack>
       <Box sx={boxSx}>
         <Typography variant="h3" sx={typoSx}>
           <strong>
-            {today}
+            <span> {month } { day }</span>
+            <span> { whatDate }</span>
           </strong>
         </Typography>
         <IconButton sx={iconButtonSx}>
@@ -45,7 +66,7 @@ function PrintDate() {
       </Box>
       <Box>
         <Typography sx={remainTaskSx}>
-          3 tasks remain
+          <strong>3 tasks remain</strong>
         </Typography>
       </Box>
     </Stack>
