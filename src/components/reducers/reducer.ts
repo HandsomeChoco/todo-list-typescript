fd48a8d4f7fe8864e387f0f4f984d7217932af87
HandsomeChoco <kkb931115@gmail.com>
2022-02-17 03:39:06 +0900

    reducer 함수 내 action type 분기점이  ACTION_TYPE enum 값을 따르게 변경

--- src/components/reducers/reducer.ts
+++ src/components/reducers/reducer.ts
@@ -1,8 +1,8 @@
-import { Action, State } from "../../type/type";
+import { Action, ACTION_TYPE, State } from "../../type/type";
 
 export function reducer(state: State, action: Action): State {
   switch (action.type) {
-    case 'ADD_ITEM':
+    case ACTION_TYPE.ADD_ITEM:
       return {
         ...state, 
         items: state.items.concat({ 
@@ -12,11 +12,11 @@ export function reducer(state: State, action: Action): State {
           text: action.text 
         })
       };
-    case 'DELETE_ITEM':
+    case ACTION_TYPE.DELETE_ITEM:
       return {
         ...state, items: state.items.filter(v => v.id !== action.id)
       };
-    case 'DONE_JOB':
+    case ACTION_TYPE.DONE_JOB:
       return { 
         ...state, items: state.items.map(v => v.id === action.id ? { ...v, isDone: !v.isDone } : v)
       }
