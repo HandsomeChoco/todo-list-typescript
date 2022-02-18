0e161f682c6813a6a5260e6ce4a3ebe4ab085003
HandsomeChoco <kkb931115@gmail.com>
2022-02-18 19:45:25 +0900

    할 일들이 들어갈 컴포넌트 디자인 및 작성

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,20 +1,64 @@
 import React, { memo } from "react";
-import { Box } from "@mui/system";
+import { Box, BoxProps } from "@mui/system";
 import { APP_CSS } from "../constants/constants";
-import { Item } from "../type/type";
+import { CheckCircleProps, ItemListProps, TodoTextProps } from "../type/type";
+import DoneIcon from '@mui/icons-material/Done';
+import { styled } from "@mui/material";
+import { grey, red } from "@mui/material/colors";
+import DeleteIcon from '@mui/icons-material/Delete';
 
-const ItemList: React.FunctionComponent<Item> = ({ id, isDone, beginAt, text }) => {
+const FlexBox = styled(Box)<BoxProps>(({ theme }) => ({
+  transition: APP_CSS.TRANSITION_TIME,
+  display: 'flex',
+  alignItems: 'center',
+  fontSize: APP_CSS.STANDARD * 4
+}));
+
+const CheckCircle = styled(Box)<CheckCircleProps>(({ theme, isDone }) => ({
+  display: 'flex', 
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: 32,
+  height: 32,
+  borderRadius: '100%',
+  border: isDone ? 'none' : `1px solid ${grey[400]}`
+}));
+
+const AppThemeDoneIcon = styled(DoneIcon)(({ theme }) => ({
+  color: APP_CSS.APP_THEME_COLOR,
+  transition: APP_CSS.TRANSITION_TIME
+}));
+
+const TodoText = styled(Box)<TodoTextProps>(({ theme, isDone }) => ({
+  display: 'flex',
+  width: '100%',
+  justifyContent: 'space-between',
+  color: isDone ? grey[500] : grey[900],
+  textDecoration: isDone ? 'line-through' : 'none',
+  paddingLeft: APP_CSS.STANDARD,
+}));
+
+const DeleteIconBox = styled(Box)<BoxProps>(({ theme }) => ({
+  color: grey[200],
+  ":hover": {
+    color: red[200],
+    cursor: 'pointer' 
+  }
+}));
+
+const ItemList: React.FunctionComponent<ItemListProps> = ({ id, isDone, beginAt, text, onToggleDone, onDelete }) => {
   return (
-    <Box
-      sx={{
-        transition: APP_CSS.TRANSITION_TIME
-      }}
-    >
-      <div>{id}</div>
-      <div>{isDone}</div>
-      <div>{beginAt}</div>
-      <div>{text}</div>
-    </Box>
+    <FlexBox>
+      <CheckCircle isDone={isDone} onClick={() => onToggleDone(id)}>
+        { isDone && <AppThemeDoneIcon/> }
+      </CheckCircle>
+      <TodoText isDone={isDone}> 
+        <Box sx={{ width: '100%' }}>{text}</Box>
+        <DeleteIconBox>
+          <DeleteIcon onClick={() => onDelete(id)}/>
+        </DeleteIconBox> 
+      </TodoText> 
+    </FlexBox>
   )
 };
 
