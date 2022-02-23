044301d4ccaf9a4691292aa301a0c25aeed45dc9
HandsomeChoco <kkb931115@gmail.com>
2022-02-23 14:37:03 +0900

    라우터 사용 시작

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App/>}/>
+      </Routes>      
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
